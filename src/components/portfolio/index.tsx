9e5334d6a321509ff4a52b52177d28955e444b86
Marsch Huynh <marsch.huynh@gmail.com>
2023-06-17 16:55:53 +0700

    chore: #wip

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -30,8 +30,20 @@ const FoodDrink = imageFactory('Food_Drink', 36) ?? []
 const Event = imageFactory('Event', 17) ?? []
 const Wedding = imageFactory('Wedding', 27) ?? []
 
-const albums = [
+type AlbumType = {
+    title: string,
+    images?: Image[],
+    link?: string
+    seeMoreLink?: string
+}
+
+const getImageUrl = (src: string) => {
+    return `https://api.bigbee.media/api/v1${src}?page=0&limit=6`
+}
+
+const albums: AlbumType[] = [
     { images: Sport, title: 'Sport' },
+    { title: 'Recent images', link: 'https://api.bigbee.media/api/v1/photo', seeMoreLink: 'https://raceapp.bigbee.media/' },
     { images: Product, title: 'Product' },
     { images: Portrait, title: 'Portrait' },
     { images: FoodDrink, title: 'Food & Drink' },
@@ -39,10 +51,11 @@ const albums = [
     { images: Wedding, title: 'Wedding' },
 ]
 
-const AlbumGrid = ({ images, name }: { images: Image[], name: string }) => {
+const AlbumGrid = ({ images: defaultImages, name, link, seeMoreLink }: { images?: Image[], name: string, link?: string, seeMoreLink?: string }) => {
     const [refText, inViewText] = useInView()
     const [refOfLastImage, inViewOfLastImage] = useInView()
     const [showMore, setShowMore] = useState(false)
+    const [images, setImages] = useState(defaultImages ?? [])
 
     const [refBelowText, inViewBelowText] = useInView({
         rootMargin: '-100px',
@@ -71,6 +84,7 @@ const AlbumGrid = ({ images, name }: { images: Image[], name: string }) => {
             mass: 5, tension: 500, friction: 100,
         },
         trail: 120,
+        deps: [images]
     }))
 
     const [animatedRestImage, animatedRestImageApi] = useTransition(images.slice(6), () => ({
@@ -88,6 +102,23 @@ const AlbumGrid = ({ images, name }: { images: Image[], name: string }) => {
             animatedRestImageApi.start({ opacity: 1, y: 0, scale: 1 })
         }
     }, [showMore])
+
+    const getImages = async (link: string) => {
+        const resp = await fetch(link)
+        const data = await resp.json()
+        const images = data.data.map((item: any) => ({
+            id: item.id,
+            src: getImageUrl(item.preview.url),
+            category: 'portfolio'
+        }))
+        setImages(images);
+    }
+
+    useEffect(() => {
+        if (link) {
+            getImages(link)
+        }
+    }, [link])
 
     useEffect(() => {
         if (inViewBelowText) {
@@ -108,25 +139,29 @@ const AlbumGrid = ({ images, name }: { images: Image[], name: string }) => {
         y: number;
         scale: number;
     }>, startIndex = 0) => {
-        return generator((style, image, state, index) => (
-            <div className={'album-item-wrapper'} key={index}>
-                <animated.div className="item" style={style} ref={index === 5 ? refOfLastImage : null}>
-                    <Image
-                        onClick={() => {
-                            setSelectedIndex(index + startIndex)
-                            setIsOpen(true)
-                        }}
-                        fill
-                        quality={100}
-                        src={image.src}
-                        alt={image.category}
-                        sizes="50vw"
-                    />
-                </animated.div>
-            </div>
-        ))
+        return generator((style, image, state, index) => {
+            console.log('Index', index)
+            return (
+                <div className={'album-item-wrapper'} key={image.id}>
+                    <animated.div className="item" style={style} ref={index === 5 ? refOfLastImage : null}>
+                        <Image
+                            onClick={() => {
+                                setSelectedIndex(index + startIndex)
+                                setIsOpen(true)
+                            }}
+                            fill
+                            quality={100}
+                            src={image.src}
+                            alt={image.category}
+                            sizes="50vw"
+                        />
+                    </animated.div>
+                </div>
+            )
+        })
     }
 
+    console.log('Render album', name, animatedImage)
     return <div className='pt--120' id={`portfolio-${name}`}>
         <animated.div ref={refText} style={textStyle} className="section-title text-center pb--30">
             <h2 ref={refBelowText} className="theme-gradient">{name}</h2>
@@ -135,7 +170,16 @@ const AlbumGrid = ({ images, name }: { images: Image[], name: string }) => {
             {renderImage(animatedImage)}
             {showMore && renderImage(animatedRestImage, 6)}
         </div>
-        {!showMore && <animated.div style={showMoreAnimatedStyle} className='show-more' onClick={() => setShowMore(prev => !prev)}>
+        {!showMore && <animated.div style={showMoreAnimatedStyle} className='show-more' onClick={() => {
+            if (seeMoreLink) {
+                window.open(
+                    seeMoreLink,
+                    '_blank' // <- This is what makes it open in a new window.
+                );
+            } else {
+                setShowMore(prev => !prev)
+            }
+        }}>
             <span>See more</span> <BsChevronDoubleDown />
         </animated.div>}
         <Viewer
@@ -156,7 +200,13 @@ const PortfolioGrid = () => {
         <div className='portfolio-wrapper'>
             {
                 albums.map((album, index) =>
-                    <AlbumGrid key={index} images={album.images} name={album.title} />)
+                    <AlbumGrid
+                        key={index}
+                        images={album.images}
+                        name={album.title}
+                        link={album.link}
+                        seeMoreLink={album.seeMoreLink}
+                    />)
             }
         </div>
     )
